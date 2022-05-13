6f570401488b9ac16c9e8c8b5038e7185e3483c3
frankguo77 <guozhi824@163.com>
2022-05-13 15:14:20 +0800

    typo.

--- user/primes.c
+++ user/primes.c
@@ -6,7 +6,6 @@ void primes() {
 
     int val;
     int p[2];
-    int tmpval;
 
     pipe(p);
     if (read(0, &val, sizeof(val)) <= 0) {
@@ -23,12 +22,13 @@ void primes() {
         close(p[1]);
         primes();
     }else{
+        int tmpval;
         close(1);
         dup(p[1]);
         close(p[0]);
         close(p[1]);    
         while (read(0, &tmpval, sizeof(tmpval)) > 0){
-            if (tempval % val != 0) {
+            if (tmpval % val != 0) {
                 write(1, &tmpval, sizeof(tmpval));
             }
         }
