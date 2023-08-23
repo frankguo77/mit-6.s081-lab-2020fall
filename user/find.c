e915526fe7c74d7591b6738725a1d85d75d5e225
frankguo77 <guozhi824@163.com>
2023-08-23 02:30:51 +0000

    finished find

--- user/find.c
+++ user/find.c
@@ -2,18 +2,89 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+void
+find(char *path, char *fn)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_FILE:
+    p = fmtname(path);
+    if (strcmp(p, fn) == 0){
+      printf("%s\n", path);
+    }
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("find: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      if ((strcmp(de.name, ".") == 0) ||
+          (strcmp(de.name,"..") == 0)){
+        continue;
+      }
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      // if(stat(buf, &st) < 0){
+      //   printf("find: cannot stat %s\n", buf);
+      //   continue;
+      // }
+      find(p, fn)
+    }
+
+    break;
+  }
+  close(fd);
+}
+
 int
 main(int argc, char *argv[])
 {
-  int i;
-
-  if (argc != 2){
-    fprintf(2, "Please enter a number!\n");
+  if (argc != 3){
+    fprintf(2, "Please enter path and filename!\n");
     exit(1);
     } else {
-      i = atoi(argv[1]);
-      sleep(i);
-      fprintf(2, "Bye! \n");
       exit(0);
     }
 }
