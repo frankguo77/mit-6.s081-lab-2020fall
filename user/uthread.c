be1353dadc015e7ca772195ad747e7837425d1f5
frankguo77 <guozhi824@163.com>
2023-10-19 06:05:48 +0000

    uthread commit 1

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,47 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  // int            lock;
+  char           stack[STACK_SIZE]; /* the thread's stack */
+  struct context context;
+  int            state;             /* FREE, RUNNING, RUNNABLE */
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
+
+// void acquire2(int* lk) {
+//   while (__sync_lock_test_and_set(lk, 1) != 0) 
+//      ;
+//   __sync_synchronize();
+// }
+
+// void release2(int* lk) {
+//    __sync_synchronize();
+
+//    __sync_lock_release(&lk);
+// }
               
 void 
 thread_init(void)
@@ -28,6 +60,12 @@ thread_init(void)
   // save thread 0's state.  thread_schedule() won't run the main thread ever
   // again, because its state is set to RUNNING, and thread_schedule() selects
   // a RUNNABLE thread.
+  // struct thread *t;
+
+  // for (t = all_thread; t < &all_thread[MAX_THREAD]; t++) {
+  //   t->lock = 0;
+  // }
+
   current_thread = &all_thread[0];
   current_thread->state = RUNNING;
 }
@@ -63,6 +101,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->context, &next_thread->context)
   } else
     next_thread = 0;
 }
@@ -77,6 +116,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  memset(&t->context, 0, sizeof(t>context));
+  t->context.sp = (uint64)t->stack;
+  t->context.ra = (uint64)func;
 }
 
 void 
